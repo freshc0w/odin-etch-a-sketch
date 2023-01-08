aca6bcba22117da545ec4c15f97add0753b1fe16
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-01-08 23:47:13 +1000

    Fix bug where squares needs to be removed first before changing size

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
 function populateBoard(size) {
     let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove()) // Delete all squares before adding again.
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`; 
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
-    for (let i = 0; i < 256; i++) {
+    let amount = size * size
+    for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
         square.style.backgroundColor = 'blue';
         board.insertAdjacentElement('beforeend', square);
@@ -16,6 +19,6 @@ function setSize(input) {
     populateBoard(input);
 };
 
-
+populateBoard(16);
 
  
